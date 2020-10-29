b74130754442f0ac14461d83761858237c0d5246
2dam3 <iker_m123@hotmail.es>
2020-10-29 09:31:19 +0100

    Segunda pantalla para guardar datos creada (Sin guardar datos)

--- lib/main.dart
+++ lib/main.dart
@@ -29,12 +29,12 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int numHorario = 4;
   int numHoras = 6;
-  List<String> listaHorario= ["8:00 -8:50","8-50:-9.50","","","",""];
-  List<String> listaDiaSemanas= ["L","M","Mi","J","V"];
+  List<String> listaHorario = ["8:00 -8:50", "8-50:-9-50", "", "", "", ""];
+  List<String> listaDiaSemanas = ["L", "M", "Mi", "J", "V"];
   @override
   Widget build(BuildContext context) {
     var container = Container(
-        width: 75,
+        width: 100,
         height: 100,
         decoration: BoxDecoration(
             border: Border.all(width: 3, color: Colors.orange),
@@ -45,7 +45,15 @@ class _MyHomePageState extends State<MyHomePage> {
             Column(
               children: [
                 Text('Primera Clase'),
-                Icon(Icons.brush),
+                RaisedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => SecondRoute()),
+                    );
+                  },
+                  child: Icon(Icons.add),
+                ),
               ],
             ),
             Column(
@@ -63,34 +71,72 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Table(children: [
         TableRow(children: [
           Text(""),
-           for (int i = 0; i <= numHorario; i++) ...{
-             Center(
-              child:Text(listaDiaSemanas[i],textAlign: TextAlign.justify),
-              ),
-
-           },
-         
-        ]),
-        for(int y =0;y<numHoras;y++)...{
-           TableRow(
-          children: [
+          for (int i = 0; i <= numHorario; i++) ...[
             Center(
-             child:Text(listaHorario[y],textAlign: TextAlign.justify),
+              child: Text(listaDiaSemanas[i], textAlign: TextAlign.justify),
             ),
-            for (int i = 0; i <= numHorario; i++) ...{
-              Column(
-                children: [
-                  container,
-                ],
-              ),
-            },
           ],
-        ),
-
-        }
-       
+        ]),
+        for (int y = 0; y < numHoras; y++) ...[
+          TableRow(
+            children: [
+              Center(
+                child: Text(listaHorario[y], textAlign: TextAlign.justify),
+              ),
+              for (int i = 0; i <= numHorario; i++) ...[
+                Column(
+                  children: [
+                    container,
+                  ],
+                ),
+              ],
+            ],
+          ),
+        ]
       ]),
     );
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final _formKey = GlobalKey<FormState>();
+    var container = Container(
+      decoration: BoxDecoration(
+          border: Border.all(width: 3, color: Colors.orange),
+          color: Colors.green,
+          borderRadius: BorderRadius.circular(15)),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text("Clase:"),
+            TextFormField(),
+            Text("Clase:"),
+            TextFormField(),
+            Text("Clase:"),
+            TextFormField(),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: BackButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                //child: Text('Guardar'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Titulo"),
+        ),
+        body: container);
   }
 }
 /*
